c2769e6f946ff8179427597a1308544bf17a377b
Heidi Sand <heisand_p@hotmail.com>
2024-06-29 13:42:04 +0200

    Add missing getters and setters for UserWithRolesDTO serialization

--- backend/src/main/java/no/cancerregistry/model/UserWithRolesDTO.java
+++ backend/src/main/java/no/cancerregistry/model/UserWithRolesDTO.java
@@ -5,11 +5,30 @@ import java.util.List;
 public class UserWithRolesDTO {
     private Long userId;
     private String name;
-    private List<UserRoleDTO> roles;
+    private List<RoleDTO> roles;
 
-    public UserWithRolesDTO(Long userId, String name, List<UserRoleDTO> roles) {
+    public UserWithRolesDTO(Long userId, String name, List<RoleDTO> roles) {
         this.userId = userId;
         this.name = name;
         this.roles = roles;
+    }
+
+    public void setId(Long id) {
+        this.userId = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setRoles(List<RoleDTO> roles) {
+        this.roles = roles;
+    }
+    public Long getId() {
+        return userId;
+    }
+    public String getName() {
+        return name;
+    }
+    public List<RoleDTO> getRoles() {
+        return roles;
     }
 }

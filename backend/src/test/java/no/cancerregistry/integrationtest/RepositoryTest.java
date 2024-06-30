3b243d01adf739a8cdc49689531e80ce8790dba1
Heidi Sand <heisand_p@hotmail.com>
2024-06-30 23:59:24 +0200

    Add more repository tests

--- backend/src/test/java/no/cancerregistry/integrationtest/RepositoryTest.java
+++ backend/src/test/java/no/cancerregistry/integrationtest/RepositoryTest.java
@@ -47,16 +47,16 @@ public class RepositoryTest {
     }
 
     @Test
-    public void createUser() {
+    public void whenCreateUser_thenReturnSavedUserWithNewId() {
         User newUser = new User();
         newUser.setVersion(1);
-        newUser.setName("Forsker Forskersen");
+        newUser.setName("Researcher Researcher");
 
         User savedUser = userRepository.save(newUser);
         User foundUser = userRepository.findById(savedUser.getId()).orElse(null);
 
         assertNotNull(foundUser);
-        assertEquals("Forsker Forskersen", foundUser.getName());
+        assertEquals("Researcher Researcher", foundUser.getName());
         assertEquals(1, foundUser.getId());
         assertEquals(1, foundUser.getVersion());
 
@@ -67,15 +67,15 @@ public class RepositoryTest {
     }
 
     @Test
-    public void getUnitUsersWithRoles() {
+    public void whenGetUnitUsersWithRoles_thenReturnUsersWithRoles() {
         User user = new User();
         user.setVersion(2);
-        user.setName("Forsker Forskersen");
+        user.setName("Researcher Researchersen");
 
         User savedUser = userRepository.save(user);
 
         Unit unit = new Unit();
-        unit.setName("Forsker");
+        unit.setName("Researcher");
         unit.setVersion(2);
 
         Unit savedUnit = unitRepository.save(unit);
@@ -87,7 +87,7 @@ public class RepositoryTest {
         unitRepository.save(unit2);
 
         Role role = new Role();
-        role.setName("Forsker");
+        role.setName("Researcher");
         role.setVersion(2);
 
         Role savedRole = roleRepository.save(role);
@@ -106,27 +106,66 @@ public class RepositoryTest {
     }
 
     @Test
-    public void overlappingRoles() {
+    public void whenGetValidUserRoles_thenReturnOnlyValidUserRoles() {
         User user = new User();
         user.setVersion(2);
-        user.setName("Forsker Forskersen");
+        user.setName("Researcher Researchersen");
 
         User savedUser = userRepository.save(user);
 
         Unit unit = new Unit();
-        unit.setName("Forsker");
+        unit.setName("Researcher");
         unit.setVersion(2);
 
         Unit savedUnit = unitRepository.save(unit);
 
-        Unit unit2 = new Unit();
-        unit.setName("Lege");
+        Role role = new Role();
+        role.setName("Researcher");
+        role.setVersion(2);
+
+        Role savedRole = roleRepository.save(role);
+
+        UserRole userRole = new UserRole();
+        userRole.setVersion(2);
+        userRole.setUser(savedUser);
+        userRole.setUnit(savedUnit);
+        userRole.setRole(savedRole);
+        userRole.setValidFrom(ZonedDateTime.now());
+        userRole.setValidTo(ZonedDateTime.now().plusDays(3));
+
+        userRoleRepository.save(userRole);
+
+        UserRole userRole2 = new UserRole();
+        userRole2.setVersion(2);
+        userRole2.setUser(savedUser);
+        userRole2.setUnit(savedUnit);
+        userRole2.setRole(savedRole);
+        userRole2.setValidFrom(ZonedDateTime.now().plusDays(4));
+        userRole2.setValidTo(ZonedDateTime.now().plusDays(5));
+
+        userRoleRepository.save(userRole2);
+
+        List<UserRole> validUserRoles = userRoleRepository.findValidUserRoles(1L, 1L, ZonedDateTime.now());
+
+        assertEquals(1, validUserRoles.size());
+    }
+
+    @Test
+    public void givenOverlappingRoles_thenFindOverlappingRoles() {
+        User user = new User();
+        user.setVersion(2);
+        user.setName("Researcher Researchersen");
+
+        User savedUser = userRepository.save(user);
+
+        Unit unit = new Unit();
+        unit.setName("Researcher");
         unit.setVersion(2);
 
-        unitRepository.save(unit2);
+        Unit savedUnit = unitRepository.save(unit);
 
         Role role = new Role();
-        role.setName("Forsker");
+        role.setName("Researcher");
         role.setVersion(2);
 
         Role savedRole = roleRepository.save(role);
@@ -144,5 +183,54 @@ public class RepositoryTest {
         boolean isOverlappingRoles = userRoleService.hasOverlappingRole(userRole);
 
         assertTrue(isOverlappingRoles);
+    }
+
+    @Test
+    public void givenNoOverlappingRoles_thenFindNoOverlappingRoles() {
+        User user = new User();
+        user.setVersion(2);
+        user.setName("Researcher Researchersen");
+
+        User savedUser = userRepository.save(user);
+
+        Unit unit = new Unit();
+        unit.setName("Researcher");
+        unit.setVersion(2);
+
+        Unit savedUnit = unitRepository.save(unit);
+
+        Unit unit2 = new Unit();
+        unit.setName("Lege");
+        unit.setVersion(2);
+
+        Unit savedUnit2 = unitRepository.save(unit2);
+
+        Role role = new Role();
+        role.setName("Researcher");
+        role.setVersion(2);
+
+        Role savedRole = roleRepository.save(role);
+
+        UserRole userRole = new UserRole();
+        userRole.setVersion(2);
+        userRole.setUser(savedUser);
+        userRole.setUnit(savedUnit);
+        userRole.setRole(savedRole);
+        userRole.setValidFrom(ZonedDateTime.now());
+        userRole.setValidTo(ZonedDateTime.now().plusDays(1));
+
+        userRoleRepository.save(userRole);
+
+        UserRole userRole2 = new UserRole();
+        userRole2.setVersion(2);
+        userRole2.setUser(savedUser);
+        userRole2.setUnit(savedUnit2);
+        userRole2.setRole(savedRole);
+        userRole2.setValidFrom(ZonedDateTime.now());
+        userRole2.setValidTo(ZonedDateTime.now().plusDays(1));
+
+        boolean isOverlappingRoles = userRoleService.hasOverlappingRole(userRole2);
+
+        assertFalse(isOverlappingRoles);
     }
 }
